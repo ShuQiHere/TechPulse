e54907d94e156e69bfb04715d77a2f781e7d5201
Shuqi <157318623+ShuQiHere@users.noreply.github.com>
2024-07-24 14:02:44 +0800

    Update login.js

--- assets/login.js
+++ assets/login.js
@@ -1,1 +1,16 @@
 
+var app = angular.module('loginApp', []);
+
+app.controller('LoginCtrl', function($scope, $window) {
+  $scope.username = '';
+  $scope.password = '';
+
+  $scope.login = function() {
+    // 简单的验证逻辑，可以根据需要进行扩展
+    if ($scope.username === 'admin' && $scope.password === 'password') {
+      $window.location.href = 'index.html';
+    } else {
+      alert('Invalid username or password');
+    }
+  };
+});
